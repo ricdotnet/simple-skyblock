4c2a5d45129d7737f0b0124545881534d7724356
Ricardo Rocha <me@rrocha.uk>
2025-11-29 22:04:12 +0000

    fix: fix spawn when bed is obstructed

---
+++ src/main/java/dev/ricr/skyblock/listeners/PlayerRespawnListener.java
@@ -0,0 +1,37 @@
+package dev.ricr.skyblock.listeners;
+
+import com.j256.ormlite.dao.Dao;
+import dev.ricr.skyblock.SimpleSkyblock;
+import dev.ricr.skyblock.database.Balance;
+import dev.ricr.skyblock.generators.IslandGenerator;
+import dev.ricr.skyblock.utils.PlayerUtils;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import java.sql.SQLException;
+
+public class PlayerRespawnListener implements Listener {
+
+    private final SimpleSkyblock plugin;
+
+    public PlayerRespawnListener(SimpleSkyblock plugin, IslandGenerator islandGenerator) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+
+        if (!event.isBedSpawn()) {
+            this.plugin.getLogger()
+                    .info(String.format("Resetting spawn for %s", player.getName()));
+
+            event.setRespawnLocation(PlayerUtils.getPlayerIslandLocation(this.plugin, event.getPlayer()));
+        }
+    }
+}
+
--- src/main/java/dev/ricr/skyblock/utils/PlayerUtils.java
+++ src/main/java/dev/ricr/skyblock/utils/PlayerUtils.java
@@ -1,12 +1,18 @@
 package dev.ricr.skyblock.utils;
 
+import dev.ricr.skyblock.SimpleSkyblock;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import java.io.File;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -15,7 +21,7 @@ import java.util.logging.Logger;
 public class PlayerUtils {
     static Logger logger = Logger.getLogger("SimpleSkyblock");
 
-    public static ItemStack getPlayerHead(UUID playerUniqueId, String ...name) {
+    public static ItemStack getPlayerHead(UUID playerUniqueId, String... name) {
         logger.info("Getting player head for " + playerUniqueId);
 
         OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerUniqueId);
@@ -23,7 +29,8 @@ public class PlayerUtils {
         ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
         SkullMeta meta = (SkullMeta) head.getItemMeta();
 
-        String displayName = name.length > 0 ? name[0] : Objects.requireNonNull(offlinePlayer.getName(), "Name cannot be null!");
+        String displayName = name.length > 0 ? name[0] : Objects.requireNonNull(offlinePlayer.getName(), "Name cannot" +
+                " be null!");
 
         if (meta != null) {
             meta.setOwningPlayer(offlinePlayer);
@@ -34,4 +41,15 @@ public class PlayerUtils {
         return head;
     }
 
+    public static Location getPlayerIslandLocation(SimpleSkyblock plugin, Player player) {
+        UUID playerUniqueId = player.getUniqueId();
+
+        File playerFile = new File(plugin.getDataFolder(), playerUniqueId + ".yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(playerFile);
+
+        return new Location(plugin.getServer()
+                .getWorld(config.getString("world")), config.getDouble("x"),
+                config.getDouble("y"), config.getDouble(
+                "z"));
+    }
 }
