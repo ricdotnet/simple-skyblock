163507573a59fa35ded7dbac07ee24fda3db8a63
Ricardo Rocha <me@rrocha.uk>
2026-01-21 13:14:47 +0000

    chore: implement click handler for playtime key chest

--- src/main/java/dev/ricr/skyblock/gui/PlayTimeChestGUI.java
+++ src/main/java/dev/ricr/skyblock/gui/PlayTimeChestGUI.java
@@ -2,12 +2,18 @@ package dev.ricr.skyblock.gui;
 
 import dev.ricr.skyblock.DisplayNames;
 import dev.ricr.skyblock.SimpleSkyblock;
+import dev.ricr.skyblock.enums.SoundType;
 import dev.ricr.skyblock.items.CreeperCoin;
 import dev.ricr.skyblock.items.LuckyPickaxe;
+import dev.ricr.skyblock.items.PlayTimeKey;
 import dev.ricr.skyblock.utils.InventoryUtils;
+import dev.ricr.skyblock.utils.PlayerUtils;
+import dev.ricr.skyblock.utils.ServerUtils;
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
@@ -31,6 +37,29 @@ public class PlayTimeChestGUI implements InventoryHolder, ISimpleSkyblockGUI {
     @Override
     public void handleInventoryClick(InventoryClickEvent event, Player player) {
         event.setCancelled(true);
+
+        var clicked = event.getCurrentItem();
+        if (clicked == null || clicked.getType() == Material.GRAY_STAINED_GLASS_PANE) {
+            return;
+        }
+
+        if (!player.getInventory().contains(PlayTimeKey.create(this.plugin))) {
+            var noKeyMessage = "<red>You need a PlayTime Key to claim this item";
+            player.sendMessage(this.plugin.miniMessage.deserialize(noKeyMessage));
+            PlayerUtils.playSound(player, SoundType.NEGATIVE);
+            return;
+        }
+
+        player.give(clicked.clone());
+
+        var successMessage = "<green>You have received a <item>";
+        player.sendMessage(this.plugin.miniMessage.deserialize(
+                successMessage,
+                Placeholder.unparsed("item", ServerUtils.getTextFromComponent(clicked.getItemMeta().displayName()))
+        ));
+        PlayerUtils.playSound(player, SoundType.POSITIVE);
+
+        this.inventory.close();
     }
 
     private void loadInventory() {
